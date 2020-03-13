791f91b11b717ffd40f8bc01ffbaa465b963cdf1
Alicia G <alicia.guardenoalbertos@gmail.com>
2020-03-13 20:19:11 +0100

    change project name & add xmlhttprequests to server api

---
+++ project_xmlhttprequest/src/API/carsApi.js
@@ -0,0 +1,53 @@
+const defaultPort = 3050;
+const baseUrl = `http://localhost:${defaultPort}/api`;
+
+export const getAllCars = () => {
+  return new Promise((resolve, reject) => {
+    const url = `${baseUrl}/cars`;
+    const client = new XMLHttpRequest();
+    client.responseType = "json";
+
+    client.onload = event => resolve(event.target.responseText);
+    client.onerror = event =>
+      reject(`${event.target.status}:${event.target.statusText}`);
+    client.open("get", url);
+    client.send();
+  });
+};
+
+export const getCarById = id => {
+  return new Promise((resolve, reject) => {
+    const url = `${baseUrl}/cars/${id}`;
+    const client = new XMLHttpRequest();
+    client.responseType = "json";
+
+    client.onload = event => resolve(event.target.responseText);
+    client.onerror = event =>
+      reject(`${event.target.status}:${event.target.statusText}`);
+    client.open("get", url);
+    client.send();
+  });
+};
+
+export const addCar = car => {
+  return new Promise((resolve, reject) => {
+    const url = `${baseUrl}/cars`;
+    const car_json = {
+      card_id: car.car_id,
+      name: car.name,
+      brand: car.brand,
+      year_release: car.year_release
+    };
+
+    const client = new XMLHttpRequest();
+    client.responseType = "json";
+    client.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+    client.onload = event => resolve(event.target.responseText);
+    client.onerror = event =>
+      reject(`${event.target.status}:${event.target.statusText}`);
+
+    client.open("post", url);
+    client.send(car_json);
+  });
+};
